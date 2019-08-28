6ea80f63b52257c8c9732be421f896c9ca4e9d63
Neeraj Kapoor <neeraj.kapoor@lootcrate.com>
2019-08-27 23:28:44 -0700

    add in api calls for submissions

---
+++ lib/paperform/submission.rb
@@ -0,0 +1,33 @@
+module Paperform
+  class Submission
+    attr_reader :token
+
+    BASE_URL = 'https://api.paperform.co/v1/submissions'
+
+    def initialize(token)
+      @token = token
+    end
+
+    def find(id)
+      params = { id: id }
+
+      response = Curl.get(BASE_URL, params) do |http|
+        http.headers['Authorization'] = "Bearer #{token}"
+      end
+
+      response.body
+    end
+
+    def list(form_id, options = nil)
+      params = { form: form_id }
+      params.merge!(options) if options
+
+      response = Curl.get(BASE_URL, params) do |http|
+        http.headers['Authorization'] = "Bearer #{token}"
+      end
+
+      response.body
+    end
+
+  end
+end
